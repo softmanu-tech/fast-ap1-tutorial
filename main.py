b09143038189503e188167acc36d8633ad2e13e8
softmanu-tech <walelae86@gmail.com>
2025-05-09 13:51:53 -0700

    initials

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ async def root():
 @app.post("/")
 async def post():
     return {"message": "Hello World from POST"}
+
+@app.put("/")
+async def put():
+    return {"message": "Hello World from PUT"}
+
+@app.delete("/")
+async def delete():
+    return {"message": "Hello World from DELETE"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
